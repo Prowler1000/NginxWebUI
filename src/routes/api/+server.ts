f88e134eeeec015be46b9d80f8908c1d37f8203a
Braeden <bclarke@fastmail.com>
2024-07-10 08:27:51 -0500

    Initial commit

---
+++ src/routes/api/+server.ts
@@ -0,0 +1,6 @@
+import type { RequestHandler } from "./$types";
+
+export const GET: RequestHandler = ({ url }) => {
+    console.log(url.searchParams);
+    return new Response(url.searchParams);
+}
