60fe1a5ce040e227bfdaea138701ee6d8f6fbd95
Braeden <bclarke@fastmail.com>
2024-08-22 19:43:25 -0500

    Properly print errors for GetAllProxies API

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -19,21 +19,27 @@ export async function GetAllServers(fetchFunc = fetch): Promise<Server[]> {
 
 export async function GetAllProxyServers(fetchFunc = fetch): Promise<(Server & ProxyServer)[]> {
     const res = await fetchFunc('api/v1/servers/Get-Proxies?includeServer=true');
-    const json = await res.json();
-    const array: (Server & ProxyServer)[] = json.map((proxy: ProxyServer & {server: Server}) => {
-        return {
-            id: proxy.id,
-            enable: proxy.server.enable,
-            name: proxy.server.name,
-            hostname: proxy.server.hostname,
-            http_port: proxy.server.http_port,
-            ssl_port: proxy.server.ssl_port,
-            use_ssl: proxy.server.use_ssl,
-            forward_scheme: proxy.forward_scheme,
-            forward_server: proxy.forward_server,
-            forward_port: proxy.forward_port,
-            serverId: proxy.serverId
-        }
-    })
-    return array;
+    if (res.status != 200) {
+        console.error(`An error occured during fetch. ${await res.text()}`)
+        return []
+    }
+    else {
+        const json = await res.json();
+        const array: (Server & ProxyServer)[] = json.map((proxy: ProxyServer & {server: Server}) => {
+            return {
+                id: proxy.id,
+                enable: proxy.server.enable,
+                name: proxy.server.name,
+                hostname: proxy.server.hostname,
+                http_port: proxy.server.http_port,
+                ssl_port: proxy.server.ssl_port,
+                use_ssl: proxy.server.use_ssl,
+                forward_scheme: proxy.forward_scheme,
+                forward_server: proxy.forward_server,
+                forward_port: proxy.forward_port,
+                serverId: proxy.serverId
+            }
+        })
+        return array;
+    }
 }
